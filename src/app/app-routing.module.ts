e0205d23dc692e61f73d020fa9a5e2a557507b5c
prateek951 <prince.cenation@gmail.com>
2019-03-15 13:25:44 +0530

    added the initial code structure for the project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+//tslint:disable
+
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+//routes go here 
+const routes: Routes = [
+  { path: "", redirectTo: "places", pathMatch: "full" },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
