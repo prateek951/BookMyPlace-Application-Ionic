0bda7c9b9589331b87bd576dfefb28938325893e
prateek951 <prince.cenation@gmail.com>
2019-03-15 15:13:14 +0530

    working on the discover places page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from './../places.service';
+import { Place } from './../place.model';
 
 @Component({
   selector: 'app-discover',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+  loadedPlaces : Place[];
 
-  constructor() { }
+  constructor(private placesService : PlacesService) { }
 
   ngOnInit() {
+    //reach out to the service to get the list of the places
+    this.loadedPlaces = this.placesService.fetchPlaces();
+    console.log(this.loadedPlaces);
   }
+
 
 }
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  
+  //set up the mock data for the list of the places 
+  
+  private _places:Place[] = [
+      new Place('p1','Manhattan Mansion',
+      'In the heart of the New York City',
+      'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042537/harkness_mansion_gagosian.jpg'
+      ,149.9),
+      new Place('p2',
+      'Taj Mahal',
+      'An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world heritage.'
+      ,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvUa3rPdQbwSfdVJ0G3fIOeHnA3vmghZheOTJwhSU_EO8cWeWUFg',
+      2099),
+      new Place('p3',
+      'The Foggy Palace',
+      'Not your average city trip'
+      ,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvUa3rPdQbwSfdVJ0G3fIOeHnA3vmghZheOTJwhSU_EO8cWeWUFg',
+      99.99)
+  ];
+
+  //Utility method to fetch the list of all the mock places
+  
+  fetchPlaces() { 
+    return [...this._places];
+  }
+
+  constructor() { }
+}
