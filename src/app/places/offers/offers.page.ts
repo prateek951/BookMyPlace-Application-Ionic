5fdae20b60a149663975de6ae530b4ff1aff0a91
prateek951 <prince.cenation@gmail.com>
2019-03-17 17:31:18 +0530

    list items now swipable

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from './../places.service';
 import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
@@ -8,11 +10,18 @@ import { Place } from '../place.model';
 })
 export class OffersPage implements OnInit {
   loadedOffers:Place[];
-  constructor(private _placesService : PlacesService) { }
+  constructor(private _placesService : PlacesService, private router: Router) { }
 
   ngOnInit() {
     // retrieve the list of all the offers that are there on oofers 
     // for places 
     this.loadedOffers = this._placesService.fetchPlaces();
   }
+
+  onEdit(id: string, slidingNavigate: IonItemSliding) { 
+    slidingNavigate.close();
+    this.router.navigate(['/','places','tabs','offers','edit',id]);
+    console.log('Editing item', id);
+  }
+
 }
