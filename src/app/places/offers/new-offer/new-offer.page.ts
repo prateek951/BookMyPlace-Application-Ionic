23ce8f232ec1a58e81e5baaf03b87e299006d47c
prateek951 <prince.cenation@gmail.com>
2019-03-19 11:35:35 +0530

    setting up the reactive form for new offer

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
+  // Set up the form group
+  form: FormGroup;
 
   constructor() { }
 
@@ -15,6 +18,29 @@ export class NewOfferPage implements OnInit {
 
 
   ngOnInit() {
+    this.form = new FormGroup({
+      // Set up the form controls
+      title : new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description : new FormControl(null, {
+        updateOn : 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price : new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(499)]
+      }),
+      dateFrom : new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
   }
 
 }
